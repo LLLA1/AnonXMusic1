d9efc7a47089a6007f62503527bca5909c3620f4
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-08-12 07:10:46 +0300

    Update config.py
    Signed-off-by: fasdxoox <163231340+fasdxoox@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LLLA1/AnonXMusic1",
+    "https://github.com/LLLA1/SAUD",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
